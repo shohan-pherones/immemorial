fe16dead16230b138b4b0b537889ceb4c58719fb
shohan-pherones <shohan.sub.56@gmail.com>
2022-11-17 00:41:32 +0600

    footer anim added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,15 @@
+import { useRef } from "react";
+import { useGsapFooterHeadline } from "../hooks/gsap";
+
 const Footer = () => {
+  const footerRef = useRef(null);
+  const footerHeadline = useRef(null);
+
+  useGsapFooterHeadline(footerHeadline, footerRef);
+
   return (
-    <section className="footer wrapper">
-      <h1>Bonjour</h1>
+    <section className="footer wrapper" ref={footerRef}>
+      <h1 ref={footerHeadline}>Bonjour</h1>
       <p>
         &copy; {new Date().getFullYear()} Immemorial. Crafted by yours truly
       </p>
