a6d526c456703c0a09b182d2212d62243547b1ef
shohan-pherones <shohan.sub.56@gmail.com>
2022-11-16 14:06:29 +0600

    hero added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="wrapper">
       <Navbar />
+      <Hero />
     </div>
   );
 };
