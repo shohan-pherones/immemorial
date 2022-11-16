b76548a5972ab5a1dd8c595567fb9b11a1eda85e
shohan-pherones <shohan.sub.56@gmail.com>
2022-11-16 15:23:04 +0600

    about added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import About from "./components/About";
 import Featured from "./components/Featured";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
@@ -8,6 +9,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Featured />
+      <About />
     </div>
   );
 };
