b6f8b7cfe8e08cf0930a657c66ff497857dabc3e
shohan-pherones <shohan.sub.56@gmail.com>
2022-11-23 13:50:18 +0600

    blogs added

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -1,10 +1,19 @@
+import SectionTitle from "./SectionTitle";
+import Blog from "./Blog";
+import { blogsArr } from "../data/blogs";
+
 const Favourites = ({ minHeight }) => {
   return (
     <section
       className="favourites wrapper"
       style={minHeight ? { minHeight: "100vh" } : null}
     >
-      Favourites
+      <SectionTitle title="Favourites" />
+      <div className="blogs">
+        {blogsArr.map((blog) => (
+          <Blog key={blog.id} blog={blog} />
+        ))}
+      </div>
     </section>
   );
 };
